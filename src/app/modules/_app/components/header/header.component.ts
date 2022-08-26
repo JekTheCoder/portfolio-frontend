a9680336580d732b83833e7c0c1a258ff580834b
Jeshua <hinostrozajeshua@gmail.com>
2022-08-26 18:14:23 -0500

    feat: header hides when its required

--- src/app/modules/_app/components/header/header.component.ts
+++ src/app/modules/_app/components/header/header.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { animations } from './header.animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnChanges {
+
+  @Input() open = false;
+  @Output() openChange = new EventEmitter<boolean>();
+
+  @HostListener('mouseover')
+  protected mouseover() { this.emitOpenState(true); }
+
+  @HostListener('mouseleave')
+  protected mouseleave() { this.emitOpenState(false); }
+  protected state?: 'open' | 'closed';
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    this.updateState();
+  }
+
+  protected updateState() {
+    this.state = this.open ? 'open' : 'closed';
+    console.log(this.state)
+  }
+
+  protected emitOpenState(state: boolean) {
+    this.open = state;
+    this.ngOnChanges();
+    this.openChange.emit(state);
   }
 
 }
