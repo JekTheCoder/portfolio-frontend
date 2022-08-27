3999ece7e2d3cbcdcf6c9721e25dbfd0f8f5cb48
Jeshua <hinostrozajeshua@gmail.com>
2022-08-27 10:29:02 -0500

    import reactive forms module

--- src/app/modules/_app/_app.module.ts
+++ src/app/modules/_app/_app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -12,6 +13,7 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule
   ],
   exports: [HeaderComponent, FooterComponent]
 })
