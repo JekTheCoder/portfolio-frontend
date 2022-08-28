7ada76f7b0028daadcb6228077dd9a9f72f26d73
Jeshua <hinostrozajeshua@gmail.com>
2022-08-28 18:40:36 -0500

    update: navigator hides text when its width is too less}

--- src/app/modules/portfolio/components/navigator/navigator.component.ts
+++ src/app/modules/portfolio/components/navigator/navigator.component.ts
@@ -1,17 +1,40 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, debounceTime, fromEvent, map, Observable, Subject, takeUntil } from 'rxjs';
+import { ResizeObservableFactoryService } from '../../services/resize-observable-factory.service';
 
 @Component({
   selector: 'app-navigator',
   templateUrl: './navigator.component.html',
   styleUrls: ['./navigator.component.scss']
 })
-export class NavigatorComponent implements OnInit {
+export class NavigatorComponent implements OnInit, OnDestroy {
 
-  @Input() points: (string | undefined | null)[] = []
+  @Input() points: string[] = []
+  private nativeElement: HTMLElement | null;
 
-  constructor() { }
+  protected showPointName$ = new BehaviorSubject<boolean>(false);
+  protected unsuscriber$ = new Subject<void>();
+
+  constructor(
+    { nativeElement }: ElementRef,
+    private resizeFactory: ResizeObservableFactoryService
+    ) { this.nativeElement = nativeElement; }
 
   ngOnInit(): void {
+    if (!this.nativeElement) return;
+    const obs$ = this.resizeFactory.createReizeObservable(this.nativeElement)
+      .pipe(
+        map(e => e[0].target as HTMLElement),
+        map(el => el.clientWidth),
+        map(width => width > 80),
+        takeUntil(this.unsuscriber$)
+    );
+    obs$.subscribe(this.showPointName$);
+  }
+
+  ngOnDestroy(): void {
+    this.unsuscriber$.next();
+    this.unsuscriber$.complete();
   }
 
 }
