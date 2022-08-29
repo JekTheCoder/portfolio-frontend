ba129935f90a11cf442093b39514184c77f9aad9
Jeshua <hinostrozajeshua@gmail.com>
2022-08-29 16:49:54 -0500

    feature: scroll to fragments based on url

--- src/app/modules/portfolio/components/portfolio/portfolio.component.spec.ts
+++ src/app/modules/portfolio/components/portfolio/portfolio.component.spec.ts
@@ -1,17 +1,39 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, Input } from '@angular/core';
+import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { PortfolioComponent } from './portfolio.component';
+
+function isInViewport(element: HTMLElement) {
+  const rect = element.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+@Component({
+  selector: 'app-navigator',
+  template: ''
+})
+class MockNavigatorComponent { @Input() fragments: any; }
 
 describe('PortfolioComponent', () => {
   let component: PortfolioComponent;
   let fixture: ComponentFixture<PortfolioComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PortfolioComponent ]
+      declarations: [ PortfolioComponent, MockNavigatorComponent ],
+      imports: [RouterTestingModule]
     })
     .compileComponents();
 
+    router = TestBed.inject(Router);
     fixture = TestBed.createComponent(PortfolioComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +42,12 @@ describe('PortfolioComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should scroll the view to the required fragment', async () => {
+    Object.values(component['sections']).forEach(element => element.style.height = '100vh');
+
+    await router.navigate(['/'], { fragment: 'about-me' });
+    expect(isInViewport(component.aboutMe.nativeElement)).toBe(true);
+    expect(isInViewport(component.contactMe.nativeElement)).toBe(false);
+  })
 });
--- src/app/modules/portfolio/components/portfolio/portfolio.component.ts
+++ src/app/modules/portfolio/components/portfolio/portfolio.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
 })
-export class PortfolioComponent implements OnInit {
+export class PortfolioComponent implements AfterViewInit, OnDestroy {
+  protected sections: { [key in string]: HTMLElement } = {};
+  protected sectionsKeys?: string[];
 
-  constructor() { }
+  @ViewChild('aboutMe') aboutMe!: ElementRef;
+  @ViewChild('projects') projects!: ElementRef;
+  @ViewChild('contactMe') contactMe!: ElementRef;
 
-  ngOnInit(): void {
+  protected unsuscriber$ = new Subject<void>();
+
+  constructor(private route: ActivatedRoute, private changeDec: ChangeDetectorRef) {}
+
+  ngAfterViewInit(): void {
+    this.sections = {
+      'about-me': this.aboutMe.nativeElement,
+      projects: this.projects.nativeElement,
+      'contact-me': this.contactMe.nativeElement,
+    };
+
+    this.sectionsKeys = Object.keys(this.sections);
+
+    this.route.fragment
+      .pipe(takeUntil(this.unsuscriber$))
+      .subscribe((fragment) => {
+        if (!fragment) return;
+        const element: HTMLElement | undefined = this.sections[fragment];
+        if (element) element.scrollIntoView();
+      });
+
+    this.changeDec.detectChanges();
   }
 
+  ngOnDestroy(): void {
+    this.unsuscriber$.next();
+    this.unsuscriber$.complete();
+  }
 }
