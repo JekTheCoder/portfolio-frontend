d4dce48f18e57a1e9ce36aa188453c8a2394899e
Jeshua <hinostrozajeshua@gmail.com>
2022-08-29 18:39:53 -0500

    contact me component created

--- src/app/modules/portfolio/portfolio.module.ts
+++ src/app/modules/portfolio/portfolio.module.ts
@@ -5,12 +5,14 @@ import { PortfolioRoutingModule } from './portfolio-routing.module';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { NavigatorComponent } from './components/navigator/navigator.component';
 import { NgLetDirective } from 'src/app/common/directives/ng-let.directive';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
 
 
 @NgModule({
   declarations: [
     PortfolioComponent,
-    NavigatorComponent
+    NavigatorComponent,
+    ContactMeComponent
   ],
   imports: [
     CommonModule,
