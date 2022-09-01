a7edd47697a3d9ba43f1ea9a109c37ac241e76b5
Jeshua <hinostrozajeshua@gmail.com>
2022-08-31 19:28:40 -0500

    resize images on window resize

--- src/app/modules/portfolio/components/project/project.component.ts
+++ src/app/modules/portfolio/components/project/project.component.ts
@@ -1,30 +1,39 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { BoxSizeService } from '../../services/box-size.service';
-import { animations } from "./project.animations";
+import { animations } from './project.animations';
 
 interface Image {
   src?: string;
-   alt?: string
+  alt?: string;
 }
 
-type State = {
-  value: 'desktop',
-  params: {
-    height: number | string
-  }
-} | {
-  value: 'mobile',
-  params: { width: number | string }
-};
+type State =
+  | {
+      value: 'desktop';
+      params: {
+        height: number | string;
+      };
+    }
+  | {
+      value: 'mobile';
+      params: { width: number | string };
+    };
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss'],
-  animations
+  animations,
 })
-export class ProjectComponent implements OnInit, AfterViewInit {
-
+export class ProjectComponent implements AfterViewInit {
   @Input() image: Image = {};
   @Input() mImage: Image = {};
 
@@ -37,52 +46,84 @@ export class ProjectComponent implements OnInit, AfterViewInit {
 
   protected imageDesktopHeight!: number;
   protected imageMobileWidth!: number;
-  constructor(private boxSize: BoxSizeService) { }
 
-  ngOnInit(): void {
-  }
+  protected textBox!: DOMRect;
+
+  constructor(private boxSize: BoxSizeService) {}
 
   ngAfterViewInit(): void {
+    this.textBox = this.text.nativeElement.getBoundingClientRect();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    if (this.state?.value !== 'mobile') this.textBox = this.text.nativeElement.getBoundingClientRect();
+    this.resizeImages();
+    this.setState(this.state!.value);
+  }
+
+  onImagesLoad() {
+    this.resizeImages();
+    this.setState('desktop');
   }
 
   resizeImages() {
     const containerBox = this.container.nativeElement.getBoundingClientRect();
-    const textBox = this.text.nativeElement.getBoundingClientRect();    
-
-    const textHeight = textBox.height * containerBox.width / textBox.width;
-    containerBox.height += textHeight * .4;
+    const textHeight =
+      (this.textBox.height * containerBox.width) / this.textBox.width;
+    containerBox.height = textHeight + 40;
 
     this.resizeDesktopImage(containerBox);
 
     containerBox.height += this.imageDesktopHeight;
-    
+
     this.resizeMobileImage(containerBox);
-    
+
     this.container.nativeElement.style.height = containerBox.height + 'px';
-    this.toggleState();
   }
 
   protected resizeDesktopImage(containerBox: DOMRect) {
     const imageDesktop = this.imageDesktop.nativeElement;
 
-    const imageDesktopHeight = this.boxSize.calculateResizeDimension(containerBox.width, imageDesktop.naturalWidth, imageDesktop.naturalHeight);
+    const imageDesktopHeight = this.boxSize.calculateResizeDimension(
+      containerBox.width,
+      imageDesktop.naturalWidth,
+      imageDesktop.naturalHeight
+    );
     imageDesktop.style.width = containerBox.width + 'px';
-    imageDesktop.style.height = imageDesktopHeight + 'px';
-
     this.imageDesktopHeight = imageDesktopHeight;
   }
 
   protected resizeMobileImage(containerBox: DOMRect) {
-    const imageMobileWidth = this.boxSize.calculateResizeDimension(containerBox.height, this.imageMobile.nativeElement.naturalHeight, this.imageMobile.nativeElement.naturalWidth);
+    const imageMobileWidth = this.boxSize.calculateResizeDimension(
+      containerBox.height,
+      this.imageMobile.nativeElement.naturalHeight,
+      this.imageMobile.nativeElement.naturalWidth
+    );
+    this.imageMobile.nativeElement.style.height = containerBox.height + 'px';
     this.imageMobileWidth = imageMobileWidth;
-    this.imageMobile.nativeElement.style.width = imageMobileWidth + 'px';
   }
 
   toggleState() {
-    this.state = this.state?.value === 'mobile' ? {
-      value: 'desktop',
-      params: { height: this.imageDesktopHeight }
-    } : { value: 'mobile', params: { width: this.imageMobileWidth } };
+    this.state =
+      this.state?.value === 'mobile'
+        ? {
+            value: 'desktop',
+            params: { height: this.imageDesktopHeight },
+          }
+        : { value: 'mobile', params: { width: this.imageMobileWidth } };
   }
 
+  protected setState(state: 'desktop' | 'mobile') {
+    this.state =
+      state === 'mobile'
+        ? {
+            value: 'mobile',
+            params: { width: this.imageMobileWidth },
+          }
+        : {
+            value: 'desktop',
+            params: { height: this.imageDesktopHeight },
+          };
+  }
 }
