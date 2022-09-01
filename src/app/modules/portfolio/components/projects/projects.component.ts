e8133e433b46f460ff3206f23c75d37647885869
Jeshua <hinostrozajeshua@gmail.com>
2022-09-01 18:09:25 -0500

    replace lorem ipsum by real content

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEarthAmerica } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
+
+  githubIcon = faGithub;
+  globeIcon = faEarthAmerica;
 
   constructor() { }
 
