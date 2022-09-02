2aebe72ae4a3c65014ac37fee92f7a9e2ac0c47c
Jeshua <hinostrozajeshua@gmail.com>
2022-09-01 21:36:01 -0500

    create: not found component

--- src/app/modules/_app/_app.module.ts
+++ src/app/modules/_app/_app.module.ts
@@ -4,13 +4,15 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NotFoundComponent
   ],
   imports: [
     CommonModule,
