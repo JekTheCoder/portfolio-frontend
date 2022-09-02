e563febe5625906a35f3cd7700da2cd606e0f0ad
Jeshua <hinostrozajeshua@gmail.com>
2022-09-01 21:19:50 -0500

    feature: more content for backend project and animations

--- src/app/modules/portfolio/components/backend-project/backend-project.component.ts
+++ src/app/modules/portfolio/components/backend-project/backend-project.component.ts
@@ -1,11 +1,29 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-backend-project',
   templateUrl: './backend-project.component.html',
-  styleUrls: ['./backend-project.component.scss']
+  styleUrls: ['./backend-project.component.scss'],
+  animations: [
+    trigger('arrow', [
+      state('up', style({ transform: 'rotate(180deg)' })),
+      state('down', style({ transform: 'rotate(0deg)' })),
+      transition('up <=> down', animate('300ms ease-in-out'))
+    ]),
+    trigger('text', [
+      state('expanded', style({ height: '*' })),
+      state('not-expanded', style({ height: 0 })),
+      transition('expanded <=> not-expanded', animate('300ms ease-in-out'))
+    ])
+  ]
 })
 export class BackendProjectComponent implements OnInit {
+
+  arrowIcon = faArrowDown;
+
+  expanded = false; 
 
   constructor() { }
 
