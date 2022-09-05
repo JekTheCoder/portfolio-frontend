a9971228117ceb1f3020842d9180bb349eefb714
Jeshua <hinostrozajeshua@gmail.com>
2022-09-05 15:21:25 -0500

    feature: get blogs from backend

--- src/app/modules/blog-search/services/blog-preview.service.ts
+++ src/app/modules/blog-search/services/blog-preview.service.ts
@@ -1,10 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { BlogPreview } from '../models/blog-preview.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogPreviewService {
 
+  API_URI = environment.API_URI;
+
   constructor(private http: HttpClient) { }
+
+  getBlogsReview(): Observable<BlogPreview[]> {
+    return this.http.get<BlogPreview[]>(`${this.API_URI}/blogs`)
+  }
 }
