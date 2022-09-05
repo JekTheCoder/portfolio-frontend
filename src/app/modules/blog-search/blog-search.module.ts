dbf3ac75066a2b9c4f9d95b30ef05782c4fabb29
Jeshua <hinostrozajeshua@gmail.com>
2022-09-05 14:18:39 -0500

    search bar created

--- src/app/modules/blog-search/blog-search.module.ts
+++ src/app/modules/blog-search/blog-search.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 import { BlogSearchRoutingModule } from './blog-search-routing.module';
 import { BlogSearchComponent } from './components/blog-search/blog-search.component';
@@ -11,7 +17,12 @@ import { BlogSearchComponent } from './components/blog-search/blog-search.compon
   ],
   imports: [
     CommonModule,
-    BlogSearchRoutingModule
+    BlogSearchRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class BlogSearchModule { }
