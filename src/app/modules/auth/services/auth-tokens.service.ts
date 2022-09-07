dec238effc6793ac4c8f2b0dc1db1c89df465bf9
Jeshua <hinostrozajeshua@gmail.com>
2022-09-06 21:39:44 -0500

    feature: refresh gets new tokens using refresh token and call set tokens

--- src/app/modules/auth/services/auth-tokens.service.ts
+++ src/app/modules/auth/services/auth-tokens.service.ts
@@ -2,36 +2,66 @@ import { Inject, Injectable, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { LOCAL_STORAGE } from '@common/providers/local-storage.provider';
 import { interval, Subject, take, takeUntil } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { TokensResponse } from '../models/tokens-response.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthTokensService implements OnDestroy {
-
   accessToken?: string;
 
   private stop$ = new Subject<void>();
 
   constructor(
     @Inject(LOCAL_STORAGE) private localStorage: Storage,
-    private http: HttpClient,
-  ) { }
+    private http: HttpClient
+  ) {}
 
   ngOnDestroy(): void {
     this.stop$.next();
     this.stop$.complete();
   }
 
-  setTokens({ accessToken, refreshToken }: { accessToken: string, refreshToken: string }) {
+  setTokens({
+    accessToken,
+    refreshToken,
+  }: {
+    accessToken: string;
+    refreshToken: string;
+  }) {
     this.accessToken = accessToken;
     this.localStorage.setItem('refresh_token', refreshToken);
 
     interval(5000)
       .pipe(take(1), takeUntil(this.stop$))
-      .subscribe();
+      .subscribe(() => this.refresh());
   }
 
   stopRefresh() {
     this.stop$.next();
+  }
+
+  private refresh() {
+    const refreshToken = this.localStorage.getItem('refresh_token');
+    if (!refreshToken) return;
+
+    this.http
+      .get<TokensResponse>(`${environment.API_URI}/auth/refresh`, {
+        headers: {
+          Authorization: `Bearer ${refreshToken}`,
+        },
+      })
+      .subscribe(({ access_token, refresh_token }) =>
+        this.setTokens({
+          accessToken: access_token,
+          refreshToken: refresh_token,
+        })
+      );
+  }
+
+  removeToken() {
+    this.stopRefresh();
+    this.localStorage.removeItem('refresh_token');
   }
 }
