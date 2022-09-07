34a5d294ffbbaee61f2827116a9a8b1a1597c511
Jeshua <hinostrozajeshua@gmail.com>
2022-09-06 21:18:59 -0500

    feature: set tokens fn stores tokens and starts an interval

--- src/app/modules/auth/services/auth-tokens.service.spec.ts
+++ src/app/modules/auth/services/auth-tokens.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
+import { LOCAL_STORAGE } from '@common/providers/local-storage.provider';
 
 import { AuthTokensService } from './auth-tokens.service';
+
+const mockLocalStorage: Partial<Storage> = {
+  getItem: jasmine.createSpy(),
+  setItem: jasmine.createSpy()
+};
 
 describe('AuthTokensService', () => {
   let service: AuthTokensService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{ provide: LOCAL_STORAGE, useValue: mockLocalStorage }]
+    });
     service = TestBed.inject(AuthTokensService);
   });
 
--- src/app/modules/auth/services/auth-tokens.service.ts
+++ src/app/modules/auth/services/auth-tokens.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LOCAL_STORAGE } from '@common/providers/local-storage.provider';
+import { interval, Subject, take, takeUntil } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthTokensService {
+export class AuthTokensService implements OnDestroy {
 
-  constructor() { }
+  accessToken?: string;
+
+  private stop$ = new Subject<void>();
+
+  constructor(
+    @Inject(LOCAL_STORAGE) private localStorage: Storage,
+    private http: HttpClient,
+  ) { }
+
+  ngOnDestroy(): void {
+    this.stop$.next();
+    this.stop$.complete();
+  }
+
+  setTokens({ accessToken, refreshToken }: { accessToken: string, refreshToken: string }) {
+    this.accessToken = accessToken;
+    this.localStorage.setItem('refresh_token', refreshToken);
+
+    interval(5000)
+      .pipe(take(1), takeUntil(this.stop$))
+      .subscribe();
+  }
+
+  stopRefresh() {
+    this.stop$.next();
+  }
 }
