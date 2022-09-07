0a74a7ea3677a1479bcf85d23edb3a22b3d28966
Jeshua <hinostrozajeshua@gmail.com>
2022-09-07 11:18:53 -0500

    update: profile icon added

--- src/app/modules/_app/components/profile-icon/profile-icon.component.ts
+++ src/app/modules/_app/components/profile-icon/profile-icon.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-profile-icon',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-icon.component.scss']
 })
 export class ProfileIconComponent implements OnInit {
+
+  profileIcon = faUser;
 
   constructor() { }
 
