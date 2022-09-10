263a026a9409cb9cf60e2bfc3f0d7bd2b10b9b9b
Jeshua <hinostrozajeshua@gmail.com>
2022-09-10 17:33:01 -0500

    feature: get profile

--- src/app/modules/_app/services/profile.service.ts
+++ src/app/modules/_app/services/profile.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { AuthTokensService } from '../../auth/services/auth-tokens.service';
+import { Observable, switchMap, of } from 'rxjs';
+import { Profile } from '../models/profile.interface';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProfileService {
+  constructor(
+    private authTokensService: AuthTokensService,
+    private http: HttpClient
+  ) {}
 
-  constructor() { }
+  readonly profilePath = environment + '/profile';
+
+  getProfile(): Observable<Profile | null> {
+    return this.authTokensService
+      .getHasAccessToken()
+      .pipe(
+        switchMap((value) =>
+          value ? this.http.get<Profile>(this.profilePath) : of(null)
+        )
+      );
+  }
 }
