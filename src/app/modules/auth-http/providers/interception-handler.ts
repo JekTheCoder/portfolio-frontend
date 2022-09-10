e7e48d8f7f8b4c35035290a3a1062ba731c3bdcd
Jeshua <hinostrozajeshua@gmail.com>
2022-09-10 18:23:14 -0500

    create: intercepting handler

--- src/app/modules/auth-http/providers/interception-handler.ts
+++ src/app/modules/auth-http/providers/interception-handler.ts
@@ -1,7 +1,7 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs";
 
-class InterceptorHandler implements HttpHandler {
+export class InterceptorHandler implements HttpHandler {
     constructor(private next: HttpHandler, private interceptor: HttpInterceptor) {}
 
     handle(req: HttpRequest<any>): Observable<HttpEvent<any>> {
