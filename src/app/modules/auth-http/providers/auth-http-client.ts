1584187b8a78b8aa20dd8513465ab2f7fa5bce2d
Jeshua <hinostrozajeshua@gmail.com>
2022-09-10 18:31:00 -0500

    update: inject custom interceptors into custom httpclient

--- src/app/modules/auth-http/providers/auth-http-client.ts
+++ src/app/modules/auth-http/providers/auth-http-client.ts
@@ -6,13 +6,15 @@ import {
   HTTP_INTERCEPTORS,
 } from '@angular/common/http';
 import { InterceptingHandler } from './intercepting-handler';
+import { AUTH_HTTP_INTERCEPTORS } from './auth-http-interceptors';
 
 @Injectable()
 export class AuthHttpClient extends HttpClient {
   constructor(
     backend: HttpBackend,
-    @Inject(HTTP_INTERCEPTORS) httpInterceptors: HttpInterceptor[]
+    @Inject(HTTP_INTERCEPTORS) httpInterceptors: HttpInterceptor[],
+    @Inject(AUTH_HTTP_INTERCEPTORS) authHttpInterceptors: HttpInterceptor[]
   ) {
-    super(new InterceptingHandler(backend, httpInterceptors));
+    super(new InterceptingHandler(backend, [httpInterceptors, authHttpInterceptors].flat()));
   }
 }
