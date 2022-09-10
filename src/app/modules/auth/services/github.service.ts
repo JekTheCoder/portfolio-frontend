66198ae9cd21bd90e112c9372fad533b707a9356
Jeshua <hinostrozajeshua@gmail.com>
2022-09-09 20:33:41 -0500

    creaed github service

---
+++ src/app/modules/auth/services/github.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, Inject } from '@angular/core';
+import { DOCUMENT } from "@angular/common";
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+
+  githubHref = 'https://github.com/login/oauth/authorize?client_id='+ environment.githubClientId + '&scope=';
+  redirectUri = '&redirect_uri='+this.document.location.origin+'/auth/github-auth'
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
+  redirectToGithubAuth(login = true, email?: boolean) {
+    let githubHref = this.githubHref.slice();
+    if (email) githubHref += 'user:email';
+
+    githubHref += this.redirectUri + '?register=' + !login;
+
+    this.document.location.href = githubHref;
+  }
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_URI: 'api.jeshuahinostroza.com'
+  API_URI: 'api.jeshuahinostroza.com',
+  githubClientId: '2641bbe040bcb0d50c27'
 };
