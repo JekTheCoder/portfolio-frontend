44334c0f39c442c461e76cf429c0bd1255b50d64
Jeshua <hinostrozajeshua@gmail.com>
2022-09-10 21:10:26 -0500

    create: auth guard

---
+++ src/app/modules/auth/guards/auth.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthTokensService } from '../services/auth-tokens.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanLoad {
+  constructor(private authTokens: AuthTokensService) {}
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return Boolean(this.authTokens.accessToken);
+  }
+}
