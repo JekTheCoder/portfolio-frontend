1a83dc552eeed156eac8778a19abc2805a27c6a8
Jeshua <hinostrozajeshua@gmail.com>
2022-09-11 10:35:26 -0500

    feature: permission guard check that user has required permissions

--- src/app/modules/auth/guards/permission.guard.spec.ts
+++ src/app/modules/auth/guards/permission.guard.spec.ts
@@ -1,16 +1,63 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { Profile } from '../../_app/models/profile.interface';
+import { ProfileService } from '../../_app/services/profile.service';
 
 import { PermissionGuard } from './permission.guard';
 
+let permissions = [{
+  id: 0,
+  name: ''
+}];
+
 describe('PermissionGuard', () => {
   let guard: PermissionGuard;
+  let mockProfileService: Partial<ProfileService> = {
+    getProfile: () => of<Partial<Profile>>({ __role__: { id: 0, permissions, name: '' } }) as any
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{ provide: ProfileService, useValue: mockProfileService }]
+    });
     guard = TestBed.inject(PermissionGuard);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+
+  describe('canLoad', () => {
+    let reqPermissions: string[] = [];
+    const mockRoute = {
+      data: {
+        permissions: reqPermissions
+      }
+    }
+
+    beforeEach(() => {
+      permissions.length = 0;
+      reqPermissions.length = 0;
+    })
+
+    it('should pass if user permissions are great', (done) => {
+      reqPermissions.push('p1');
+      permissions.push({ id: 0, name: 'p1' });
+  
+      guard.canLoad(mockRoute, null as any).subscribe(load => {
+        expect(load).toBeTruthy();
+        done()
+      })
+    });
+
+    it('should not pass if user does not has required permissions', (done) => {
+      reqPermissions.push('p1');
+      permissions.push({ id: 0, name: 'p2' });
+
+      guard.canLoad(mockRoute, null as any).subscribe(load => {
+        expect(load).toBeFalsy();
+        done();
+      })
+    });
   });
 });
--- src/app/modules/auth/guards/permission.guard.ts
+++ src/app/modules/auth/guards/permission.guard.ts
@@ -1,19 +1,35 @@
 import { inject, Injectable } from '@angular/core';
 import { CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
 import { map, Observable } from 'rxjs';
+import { Profile } from '../../_app/models/profile.interface';
 import { ProfileService } from '../../_app/services/profile.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PermissionGuard implements CanLoad {
-
   constructor(private profile: ProfileService) {}
 
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean | UrlTree> {
-      const permissions: string[] = route.data?.['permissions'] || [];
-      return this.profile.getProfile().pipe(map(profile => !!profile));
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean | UrlTree> {
+    const permissions: string[] = route.data?.['permissions'] || [];
+    
+    if (permissions.length === 0) console.warn('Not permissions required. This should be an error')  
+
+    return this.profile
+      .getProfile()
+      .pipe(map((profile) => this.hasPemissions(permissions, profile)));
+  }
+
+  private hasPemissions(
+    requiredPermissions: string[],
+    profile?: Profile | null
+  ): boolean {
+    const userPermissions =
+      profile?.__role__.permissions.map((p) => p.name) || [];
+    for (const permission of requiredPermissions) {
+      if (!userPermissions.includes(permission)) return false;
+    }
+
+    return true;
   }
 }
