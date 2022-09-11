b0be4196549528f36cdeb4bf4cdf721ba1f08331
Jeshua <hinostrozajeshua@gmail.com>
2022-09-10 20:54:43 -0500

    change profile panel if user is authenticated

--- src/app/modules/_app/components/profile-icon/profile-icon.component.ts
+++ src/app/modules/_app/components/profile-icon/profile-icon.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { animations } from "./profile-icon.animations";
+import { Observable, map } from "rxjs";
+import { ProfileService } from '../../services/profile.service';
 
 @Component({
   selector: 'app-profile-icon',
@@ -15,7 +17,11 @@ export class ProfileIconComponent implements OnInit {
   protected overIcon = false;
   protected overPanel = false;
 
-  constructor() { }
+  protected isLogged$?: Observable<boolean>;
+
+  constructor(proService: ProfileService) {
+    this.isLogged$ = proService.getProfile().pipe(map(profile => Boolean(profile)));
+  }
 
   ngOnInit(): void {
   }
