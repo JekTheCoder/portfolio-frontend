e2d32443074f684c07ca5072043603ef9ef24a24
Jeshua <hinostrozajeshua@gmail.com>
2022-09-11 09:07:24 -0500

    cache profile response

--- src/app/modules/_app/services/profile.service.ts
+++ src/app/modules/_app/services/profile.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { AuthTokensService } from '../../auth/services/auth-tokens.service';
-import { Observable, switchMap, of } from 'rxjs';
+import { Observable, switchMap, of, shareReplay } from 'rxjs';
 import { Profile } from '../models/profile.interface';
 import { environment } from 'src/environments/environment';
 import { AuthHttpClient } from '../../auth-http/providers/auth-http-client';
+
+const CACHE_SIZE = 1;
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProfileService {
+  private profileCache$?: Observable<Profile>;
+
   constructor(
     private authTokensService: AuthTokensService,
     private http: AuthHttpClient
@@ -19,10 +23,14 @@ export class ProfileService {
   getProfile(): Observable<Profile | null> {
     return this.authTokensService
       .getHasAccessToken()
-      .pipe(
-        switchMap((value) =>
-          value ? this.http.get<Profile>(this.profilePath) : of(null)
-        )
-      );
+      .pipe(switchMap((value) => (value ? this.getCachedProfile() : of(null))));
+  }
+
+  private getCachedProfile(): Observable<Profile> {
+    if (!this.profileCache$)
+      this.profileCache$ = this.http
+        .get<Profile>(this.profilePath)
+        .pipe(shareReplay(CACHE_SIZE));
+    return this.profileCache$;
   }
 }
