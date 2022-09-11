758098d10fa8857c5467b5d95abc6b29e4230f15
Jeshua <hinostrozajeshua@gmail.com>
2022-09-11 09:34:49 -0500

    update: register profile protperties

--- src/app/modules/_app/models/profile.interface.ts
+++ src/app/modules/_app/models/profile.interface.ts
@@ -1,3 +1,18 @@
 export interface Profile {
-    
+    id: unknown,
+    username: string,
+    name?: string | null,
+    lastname?: string | null, 
+    email?: string | null,
+    roleId: unknown,
+    createdAt: Date,
+
+    /**
+     * @Deprecated
+     */
+    __role__: {
+        id: unknown,
+        name: string,
+        permissions: { id: unknown, name: string }
+    }
 }
