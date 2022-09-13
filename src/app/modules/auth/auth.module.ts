422f16de7eeb56537351626c35e241f39b4b6fc4
Jeshua <hinostrozajeshua@gmail.com>
2022-09-13 17:12:54 -0500

    import mat stepper

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
+
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { GithubAuthComponent } from './components/github-auth/github-auth.component';
 import { RegisterComponent } from './components/register/register.component';
 
@@ -17,6 +19,7 @@ import { RegisterComponent } from './components/register/register.component';
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
+    MatStepperModule
   ],
   declarations: [LoginComponent, GithubAuthComponent, RegisterComponent],
 })
