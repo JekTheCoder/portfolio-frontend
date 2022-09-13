67b76f7947cb2b58087f912d6671e0dda1711008
Jeshua <hinostrozajeshua@gmail.com>
2022-09-13 11:12:04 -0500

    create: comment component

--- src/app/modules/blog/blog.module.ts
+++ src/app/modules/blog/blog.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { BlogRoutingModule } from './blog-routing.module';
 import { BlogComponent } from './components/blog/blog.component';
+import { CommentsComponent } from './components/comments/comments.component';
 
 
 @NgModule({
   declarations: [
-    BlogComponent
+    BlogComponent,
+    CommentsComponent
   ],
   imports: [
     CommonModule,
