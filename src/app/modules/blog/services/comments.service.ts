871b15dff3a58522afa99bb6398984788ff44138
Jeshua <hinostrozajeshua@gmail.com>
2022-09-13 15:24:54 -0500

    create: comments service

--- src/app/modules/blog/services/comments.service.ts
+++ src/app/modules/blog/services/comments.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { AuthHttpClient } from '../../auth-http/providers/auth-http-client';
+import { environment } from 'src/environments/environment';
+import { Comment } from '../models/comment';
+
+const COMMENT_CREATE = environment.API_URI + '/comments/';
+
+interface CommentDto {
+  content: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentsService {
 
-  constructor() { }
+  constructor(private http: AuthHttpClient) { }
+
+  createComment(comment: CommentDto, blogId: number) {
+    return this.http.post<Comment>(COMMENT_CREATE + blogId, comment)
+  }
 }
