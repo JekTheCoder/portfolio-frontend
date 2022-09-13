f844c06ee1da768d2519ecb74a0ed59240453f7c
Jeshua <hinostrozajeshua@gmail.com>
2022-09-13 17:57:56 -0500

    add same as validator

--- src/app/modules/auth/components/register/register.component.ts
+++ src/app/modules/auth/components/register/register.component.ts
@@ -1,26 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { SameAs } from '../../validators/same-as.validator';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-
   protected registerForm = new FormGroup({
-    username: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
-    password: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
-    repeatPassword: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    username: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    password: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    repeatPassword: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
     email: new FormControl('', Validators.email),
     name: new FormControl(''),
-    lastname: new FormControl('')
+    lastname: new FormControl(''),
   });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.registerForm.controls.repeatPassword.addValidators(
+      SameAs(this.registerForm.controls.password)
+    );
   }
+
+  ngOnInit(): void {}
 
   submit() {}
 }
