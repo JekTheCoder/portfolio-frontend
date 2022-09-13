f603b73eb671516d2fc05f8b35c42c62828a461c
Jeshua <hinostrozajeshua@gmail.com>
2022-09-13 18:10:59 -0500

    import module

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -5,6 +5,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatStepperModule } from '@angular/material/stepper';
 
+import { MaterialFileInputModule } from 'ngx-material-file-input';
+
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -19,7 +21,8 @@ import { RegisterComponent } from './components/register/register.component';
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
-    MatStepperModule
+    MatStepperModule,
+    MaterialFileInputModule
   ],
   declarations: [LoginComponent, GithubAuthComponent, RegisterComponent],
 })
