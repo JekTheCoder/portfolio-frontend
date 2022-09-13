979081e7241960766f4d044663174828eb98165c
Jeshua <hinostrozajeshua@gmail.com>
2022-09-13 18:28:05 -0500

    feature register user sends profile image if its needed

--- src/app/modules/auth/services/login.service.ts
+++ src/app/modules/auth/services/login.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { map } from 'rxjs';
+import { map, switchMap } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
+import { AuthHttpClient } from '../../auth-http/providers/auth-http-client';
 import { TokensResponse } from '../models/tokens-response.interface';
 import { AuthTokensService } from './auth-tokens.service';
 
@@ -12,35 +13,52 @@ export interface LoginDto {
   password: string;
 }
 
+export interface RegisterDto extends LoginDto {
+  lastname: string;
+  name: string;
+  email?: string | null;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
   constructor(
     private authTokenService: AuthTokensService,
-    private http: HttpClient
+    private http: HttpClient,
+    private authHttp: AuthHttpClient
   ) {}
 
   private handleTokens(tokens: TokensResponse) {
     const { token, refreshToken } = tokens;
     this.authTokenService.setTokens({
       accessToken: token,
-      refreshToken
+      refreshToken,
     });
   }
 
   private createLoginObs(path: string, body: object) {
-    return this.http.post<TokensResponse>(environment.API_URI + path, body).pipe(
-      map((tokens) => this.handleTokens(tokens))
-    );
+    return this.http
+      .post<TokensResponse>(environment.API_URI + path, body)
+      .pipe(map((tokens) => this.handleTokens(tokens)));
   }
 
   login(loginDto: LoginDto) {
     return this.createLoginObs('/auth/login', loginDto);
   }
 
-  register(registerDto: any) {
-    return this.createLoginObs('/auth/register', registerDto);
+  register(registerDto: RegisterDto, profile?: File) {
+    const obs$ = this.createLoginObs('/auth/register', registerDto);
+    if (!profile) return obs$;
+
+    const formData = new FormData();
+    formData.append('profile', profile, profile.name);
+
+    return obs$.pipe(
+      switchMap(user =>
+        this.authHttp.post(environment.API_URI + '/profile/image', formData)
+      )
+    );
   }
 
   loginGithub(code: string) {
