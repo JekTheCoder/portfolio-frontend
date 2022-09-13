42f9e8049e1c027244772391101d21b1ae60bb58
Jeshua <hinostrozajeshua@gmail.com>
2022-09-12 22:45:19 -0500

    detect if id is nan

--- src/app/modules/blog/components/blog/blog.component.ts
+++ src/app/modules/blog/components/blog/blog.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable, switchMap, of, tap } from 'rxjs';
+import { BlogsService } from '../../service/blogs.service';
+import { Blog } from '../../models/blog';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+  styleUrls: ['./blog.component.scss'],
 })
-export class BlogComponent implements OnInit {
+export class BlogComponent {
+  protected blog$?: Observable<Blog | null>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private blogServ: BlogsService,
+    route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.blog$ = route.params.pipe(
+      switchMap((params) => this.getBlog(params)),
+      tap((blog) => this.redirectIfNull(blog))
+    );
   }
 
+  private getBlog(params: { [key in string]?: string }) {
+    const { id } = params;
+    const idN = Number(id);
+
+    if (Number.isNaN(idN)) return of(null);
+    return this.blogServ.getBlog(idN);
+  }
+
+  private redirectIfNull(data: unknown | null) {
+    if (!data) this.router.navigate(['/blog/search']);
+  }
 }
