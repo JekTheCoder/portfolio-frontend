a1f08cbe07dd247216d707876ec4cd7d029015e3
Jeshua <hinostrozajeshua@gmail.com>
2022-09-13 18:58:45 -0500

    feature: check if user exists obs

--- src/app/modules/auth/services/username.service.ts
+++ src/app/modules/auth/services/username.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Observable, catchError, of, map } from 'rxjs';
+
+const userExistsRoute = environment.API_URI + '/profiles/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsernameService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  usernameExists(username: string): Observable<boolean> {
+    return this.http.get<void>(userExistsRoute + username).pipe(
+      map(() => true),
+      catchError(() => of(false))
+    )
+  }
 }
