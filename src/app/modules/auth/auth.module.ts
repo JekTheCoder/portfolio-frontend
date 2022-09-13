2e331ff53d62ee9620495604652d8af1c33d3bbb
Jeshua <hinostrozajeshua@gmail.com>
2022-09-13 18:54:00 -0500

    create username service

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -13,6 +13,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { GithubAuthComponent } from './components/github-auth/github-auth.component';
 import { RegisterComponent } from './components/register/register.component';
 import { MatIconModule } from '@angular/material/icon';
+import { UsernameService } from './services/username.service';
 
 @NgModule({
   imports: [
@@ -27,5 +28,6 @@ import { MatIconModule } from '@angular/material/icon';
     MatIconModule
   ],
   declarations: [LoginComponent, GithubAuthComponent, RegisterComponent],
+  providers: [UsernameService]
 })
 export class AuthModule {}
