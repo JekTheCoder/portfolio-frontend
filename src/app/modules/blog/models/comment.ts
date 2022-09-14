3497836f9ed4f37d2a8a239ad39fb005d67be1b4
Jeshua <hinostrozajeshua@gmail.com>
2022-09-13 19:51:15 -0500

    load profile images of comments users

--- src/app/modules/blog/models/comment.ts
+++ src/app/modules/blog/models/comment.ts
@@ -1,7 +1,11 @@
+import { Profile } from "../../_app/models/profile.interface";
+
 export interface Comment {
     id: unknown,
     content: string,
     edited: boolean,
     userId: unknown,
-    blogId: unknown
+    blogId: unknown,
+
+    user: Profile
 }
