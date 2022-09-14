4fabdc6a9678e1d56471b4194252a8e3e590782d
Jeshua <hinostrozajeshua@gmail.com>
2022-09-13 20:03:22 -0500

    feature: build github auth route builder

--- src/app/modules/_app/services/github-auth-builder.service.ts
+++ src/app/modules/_app/services/github-auth-builder.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Injectable, Inject } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubAuthBuilderService {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
+  getGithubAuthRoute(register: boolean, withEmail: boolean): string {
+    return `/auth/github-auth?register=${register}&email=${withEmail}&redirect_to=${this.document.location.href}`;
+  }
 }
