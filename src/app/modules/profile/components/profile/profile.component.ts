737cfffca783de742385d50d9f4e570d962ea091
Jeshua <hinostrozajeshua@gmail.com>
2022-09-14 18:43:17 -0500

    profile header

--- src/app/modules/profile/components/profile/profile.component.ts
+++ src/app/modules/profile/components/profile/profile.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Profile } from 'src/app/modules/_app/models/profile.interface';
+import { ProfileService } from 'src/app/modules/_app/services/profile.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  protected profile$?: Observable<Profile | null>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(profile: ProfileService) {
+    this.profile$ = profile.getProfile();
   }
 
+  ngOnInit(): void {}
 }
--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './components/profile/profile.component';
+import { NgLetDirective } from '@common/directives/ng-let.directive';
+import { ProfileIconComponent } from '@common/components/profile-icon/profile-icon.component';
 
 
 @NgModule({
@@ -11,7 +13,9 @@ import { ProfileComponent } from './components/profile/profile.component';
   ],
   imports: [
     CommonModule,
-    ProfileRoutingModule
+    ProfileRoutingModule,
+    NgLetDirective,
+    ProfileIconComponent
   ]
 })
 export class ProfileModule { }
