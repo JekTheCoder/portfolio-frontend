2dc12a35f776527887b4992b0b676c5186cfeb5c
Jeshua <hinostrozajeshua@gmail.com>
2022-11-11 18:39:06 -0500

    header fix: do not disappear header on hide

--- src/app/common/animations/up-down.animation.ts
+++ src/app/common/animations/up-down.animation.ts
@@ -3,13 +3,13 @@ import { cubicBezier } from "./cubic-bezier.time";
 
 function UpDown(ms: number | string) {
     return trigger('upDown', [
-        state('true', style({ display: '*' })),
-        state('false', style({ display: 'none' })),
+        state('true', style({ })),
+        state('false', style({ opacity: 0 })),
         transition('true => false', [
             animate(cubicBezier(ms), style({ transform: 'translateY(-100%)' }))
         ]),
         transition('false => true', [
-            style({ transform: 'translateY(-100%)', display: '*' }),
+            style({ transform: 'translateY(-100%)', display: '*', opacity: 1 }),
             animate(cubicBezier(ms), style({ transform: 'translateY(0)' }))
         ])
     ]);
