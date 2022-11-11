d6893cc8e4765dbb206e2e6d3ac54cf47b87d7e2
Jeshua <hinostrozajeshua@gmail.com>
2022-11-11 18:29:45 -0500

    set tailwind custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
+function withOpacityValue(variable) {
+  return ({opacityValue}) => {
+    if (opacityValue === undefined) {
+      return `rgb(var(${variable}))`;
+    }
+    return `rgba(var(${variable}), ${opacityValue})`;
+  };
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        accent: 'var(--color-accent)',
+        warn: 'var(--color-warn)',
+        background: 'var(--color-background)',
+      }
+    },
   },
   plugins: [],
 }
