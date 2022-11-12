eac3fe2ce6cd27d9af088326e6681d756f86eae4
Jeshua <hinostrozajeshua@gmail.com>
2022-11-12 13:39:34 -0500

    tech section: set min height

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -1,14 +1,5 @@
-import {
-  AfterViewInit,
-  ChangeDetectorRef,
-  Component,
-  ElementRef,
-  OnDestroy,
-  ViewChild,
-} from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
 import { SmoothAnchorService } from '@lib/directives/smooth-anchor/smooth-anchor.service';
-import { Subject, takeUntil } from 'rxjs';
 import { DarkModeService } from 'src/app/modules/_app/services/dark-mode.service';
 
 @Component({
@@ -17,46 +8,8 @@ import { DarkModeService } from 'src/app/modules/_app/services/dark-mode.service
   styleUrls: ['./portfolio.component.scss'],
   providers: [SmoothAnchorService]
 })
-export class PortfolioComponent implements AfterViewInit, OnDestroy {
-  protected sections: { [key in string]: HTMLElement } = {};
-  protected sectionsKeys?: string[];
-
-  @ViewChild('aboutMe', { static: true }) aboutMe!: ElementRef;
-  @ViewChild('projects') projects!: ElementRef;
-  @ViewChild('contactMe') contactMe!: ElementRef;
-
-  protected unsuscriber$ = new Subject<void>();
-
+export class PortfolioComponent {
   isDark$ = this.dark.getDarkMode$();
 
-  constructor(
-    private route: ActivatedRoute, 
-    private changeDec: ChangeDetectorRef,
-    private dark: DarkModeService
-  ) {}
-
-  ngAfterViewInit(): void {
-    this.sections = {
-      'about-me': this.aboutMe.nativeElement,
-      projects: this.projects.nativeElement,
-      'contact-me': this.contactMe.nativeElement,
-    };
-
-    this.sectionsKeys = Object.keys(this.sections);
-
-    this.route.fragment
-      .pipe(takeUntil(this.unsuscriber$))
-      .subscribe((fragment) => {
-        if (!fragment) return;
-        const element: HTMLElement | undefined = this.sections[fragment];
-        if (element) element.scrollIntoView();
-      });
-
-    this.changeDec.detectChanges();
-  }
-
-  ngOnDestroy(): void {
-    this.unsuscriber$.next();
-    this.unsuscriber$.complete();
-  }
+  constructor(private dark: DarkModeService) {}
 }
