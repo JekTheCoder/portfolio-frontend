4f5f3b93f93bd1762e2f2ac40aa3bd5238ed65bf
Jeshua <hinostrozajeshua@gmail.com>
2022-11-12 09:46:17 -0500

    backend-project refactor: separate animations - use tailwind - fix overflow

--- src/app/pages/portfolio/sections/backend-project/backend-project.component.ts
+++ src/app/pages/portfolio/sections/backend-project/backend-project.component.ts
@@ -1,34 +1,23 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { faArrowDown, faGlobeAmericas } from '@fortawesome/free-solid-svg-icons';
+import { expanded300 } from '@lib/animations/expanded';
+import { flipped300 } from '@lib/animations/flipped';
 
 @Component({
   selector: 'app-backend-project',
   templateUrl: './backend-project.component.html',
   styleUrls: ['./backend-project.component.scss'],
   animations: [
-    trigger('arrow', [
-      state('up', style({ transform: 'rotate(180deg)' })),
-      state('down', style({ transform: 'rotate(0deg)' })),
-      transition('up <=> down', animate('300ms ease-in-out'))
-    ]),
-    trigger('text', [
-      state('expanded', style({ height: '*' })),
-      state('not-expanded', style({ height: 0 })),
-      transition('expanded <=> not-expanded', animate('300ms ease-in-out'))
-    ])
-  ]
+    flipped300,
+    expanded300
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BackendProjectComponent implements OnInit {
+export class BackendProjectComponent {
 
   arrowIcon = faArrowDown;
   globeIcon = faGlobeAmericas;
 
   expanded = false; 
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
