65af03f98070413ffedd16a4326fd3ebc7970b01
Jeshua <hinostrozajeshua@gmail.com>
2022-11-12 14:26:15 -0500

    tech card: right tilt animation

--- src/app/pages/portfolio/generic/tech-card/tech-card.component.ts
+++ src/app/pages/portfolio/generic/tech-card/tech-card.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ViewChild, ElementRef } from '@angular/core';
+import { rightTilt100 } from '@lib/animations/right-tilt';
+import { Observable, fromEvent, merge, map } from 'rxjs';
 
 @Component({
   selector: 'app-tech-card',
   templateUrl: './tech-card.component.html',
-  styleUrls: ['./tech-card.component.scss']
+  styleUrls: ['./tech-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [rightTilt100]
 })
 export class TechCardComponent implements OnInit {
+
+  @ViewChild('hover', { static: true }) hover!: ElementRef<HTMLElement>;
+
+  hover$!: Observable<boolean>;
 
   constructor() { }
 
   ngOnInit(): void {
+    const mouseenter = fromEvent(this.hover.nativeElement, 'mouseenter');
+    const mouseleave = fromEvent(this.hover.nativeElement, 'mouseleave');
+
+    this.hover$ = merge(
+      mouseenter.pipe(map(() => true)),
+      mouseleave.pipe(map(() => false))
+    );
   }
 
 }
---
+++ src/lib/animations/right-tilt.ts
@@ -0,0 +1,14 @@
+import { animate, state, style, transition, trigger } from "@angular/animations";
+import { cubicBezier } from "@common/animations/cubic-bezier.time";
+
+export function rightTitlt(ms: string | number) {
+    return trigger('rightTilt', [
+        state('true', style({
+            'transform-origin': '50% 80%',
+            'transform': 'rotate(5deg)'
+        })),
+        transition('true <=> *', animate(cubicBezier(ms)))
+    ]);
+}
+
+export const rightTilt100 = rightTitlt(100);
