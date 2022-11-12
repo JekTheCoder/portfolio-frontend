23e3b9d53c12410f016f6ed2b331044c901bb90c
Jeshua <hinostrozajeshua@gmail.com>
2022-11-11 20:39:40 -0500

    skills fix: remove container class to avoid conflicts with tailwind

--- src/app/pages/portfolio/sections/skills/skills.component.ts
+++ src/app/pages/portfolio/sections/skills/skills.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  styleUrls: ['./skills.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SkillsComponent implements OnInit {
 
