4f23d6a778a4cc0a78e9c35df9bb8ad5522ede0e
Jeshua <hinostrozajeshua@gmail.com>
2022-11-13 14:27:52 -0500

    remove linked-elements-router

--- projects/linked-elements/src/lib/route-matcher-strategy/base-route-matcher.strategy.ts
+++
@@ -1,5 +0,0 @@
-import { ActivatedRouteSnapshot } from "@angular/router";
-
-export abstract class BaseRouteMatcherStrategy {
-    abstract match(route: ActivatedRouteSnapshot): string | undefined | null;
-}
