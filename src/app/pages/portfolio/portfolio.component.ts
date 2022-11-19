9a5155845f559db4555f84af3134ffa63fd81a8e
Jeshua <hinostrozajeshua@gmail.com>
2022-11-18 22:39:24 -0500

    replace linked-elements services

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { LinkedElementsService } from 'linked-elements';
+import { LinkedElementsDirective } from 'linked-elements';
 import { DarkModeService } from 'src/app/modules/_app/services/dark-mode.service';
 
 @Component({
 	selector: 'app-portfolio',
 	templateUrl: './portfolio.component.html',
 	styleUrls: ['./portfolio.component.scss'],
-	providers: [LinkedElementsService],
+	hostDirectives: [LinkedElementsDirective],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PortfolioComponent {
