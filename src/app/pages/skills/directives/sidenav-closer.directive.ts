47040224e4d4baa12eef631f4edc31e486ac8e60
Jeshua <hinostrozajeshua@gmail.com>
2022-11-22 17:23:35 -0500

    sidenav closer: use host observable

--- src/app/pages/skills/directives/sidenav-closer.directive.ts
+++ src/app/pages/skills/directives/sidenav-closer.directive.ts
@@ -1,24 +1,20 @@
-import { Directive, OnDestroy } from '@angular/core';
+import { Directive } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { SidenavService } from '@lib/components/sidenav/sidenav.service';
-import { Subject, filter, tap, takeUntil } from 'rxjs';
+import { HostObservable } from 'ngx-host-observable';
+import { filter, map, Observable } from 'rxjs';
 
 @Directive({
 	selector: '[appSidenavCloser]',
 })
-export class SidenavCloserDirective implements OnDestroy {
-	private unsuscriber$ = new Subject<void>();
+export class SidenavCloserDirective {
+	@HostObservable()
+	closer$?: Observable<void>;
 
 	constructor(private router: Router, private sidenav: SidenavService) {
-		this.router.events.pipe(
-			takeUntil(this.unsuscriber$),
+		this.closer$ = this.router.events.pipe(
 			filter(event => event instanceof NavigationEnd),
-			tap(() => this.sidenav.closeSidenav())
-		)
-		.subscribe();
-	}
-
-	ngOnDestroy(): void {
-		this.unsuscriber$.next();
+			map(() => this.sidenav.closeSidenav())
+		);
 	}
 }
