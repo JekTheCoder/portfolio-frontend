a41324acaa2492b2c6cc3f178f9263946d8f84a9
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2023-12-31 21:23:20 -0500

    feat(home): include blog link in porfolio presentation

--- src/app/pages/portfolio/sections/presentation/presentation.component.ts
+++ src/app/pages/portfolio/sections/presentation/presentation.component.ts
@@ -1,12 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
 	selector: 'app-presentation',
 	templateUrl: './presentation.component.html',
 	styleUrls: ['./presentation.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PresentationComponent implements OnInit {
-	constructor() {}
-
-	ngOnInit(): void {}
-}
+export class PresentationComponent {}
